4a5595d320827b722e11dfc45310cbc053358f50
Shreyas Rajiv <131350902+shreyasrajiv327@users.noreply.github.com>
2023-11-06 13:48:42 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-# This is a simple program that adds two numbers and prints the result.
-
-A = 4  # A is a constant, so use uppercase naming.
-B = 10  # B is a constant, so use uppercase naming.
-
-result = A + B
-print(result)
